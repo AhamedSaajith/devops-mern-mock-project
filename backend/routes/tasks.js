18acdc8edd9839257da26ac98edd002a3a2abe8a
Ahamed Saajith <140696897+AhamedSaajith@users.noreply.github.com>
2025-12-24 16:47:21 +0530

    Changed tasks.js file

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -5,8 +5,8 @@ const Task = require("../models/Task");
 // GET all tasks
 router.get("/", async (req, res) => {
   const tasks = await Task.find();
-  res.json({ message: "Data recieved", tasks });
-  // res.json(tasks);
+  // res.json({ message: "Data recieved", tasks });
+   res.json(tasks);
 });
 
 // POST create task
