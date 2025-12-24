7b6877cc91a0a046cc855fc8933f224f0e2b097c
Ahamed Saajith <140696897+AhamedSaajith@users.noreply.github.com>
2025-12-24 16:54:34 +0530

    Fixed tsks.tes.js file

--- backend/tests/tasks.test.js
+++ backend/tests/tasks.test.js
@@ -2,16 +2,19 @@ const request = require("supertest");
 const { server, app } = require("../index");
 const mongoose = require("mongoose");
 
-describe("GET api/tasks", () => {
-  it("it should return 200 ok", async () => {
+describe("GET /api/tasks", () => {
+  it("should return 200 OK", async () => {
     const res = await request(app).get("/api/tasks");
     expect(res.statusCode).toBe(200);
   });
-  it("it should return object and tasks property ok", async () => {
+
+  it("should return an array of tasks", async () => {
     const res = await request(app).get("/api/tasks");
-    expect(typeof res.body).toBe("object");
-    expect(res.body).toHaveProperty("tasks");
-    console.log(res.body.tasks, 'DATA SEEDED')
+
+    // res.body is now an array
+    expect(Array.isArray(res.body)).toBe(true);
+
+    console.log(res.body, "DATA SEEDED");
   });
 });
 
